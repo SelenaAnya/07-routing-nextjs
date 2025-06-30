1e006e7b080577dce14102702fcfbaecaf4b8b29
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-06-30 20:33:35 +0300

    3006 2033

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -3,12 +3,12 @@ import NotesClient from './Notes.client';
 
 export default async function NotesPage() {
   const initialData = await fetchNotes(1, '');
-  
+
   return (
     <div>
       <h1>Notes</h1>
       <br />
-      <NotesClient initialData={initialData} />
+      <NotesClient initialData={initialData} selectedTag="All" />
     </div>
   );
 }
