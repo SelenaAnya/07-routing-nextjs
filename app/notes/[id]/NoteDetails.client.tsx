fee98f23381fce3c949b906d8d8247d28dab659c
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-04 21:06:37 +0300

    0407 2106

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -5,6 +5,7 @@ import React, { useMemo } from 'react';
 import css from './NoteDetails.module.css';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
+import Error from '@/components/Error/Error';
 
 const NoteDetailsClient = () => {
   const { id } = useParams<{ id: string }>();
@@ -16,38 +17,45 @@ const NoteDetailsClient = () => {
   } = useQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(Number(id)),
+    refetchOnMount: false,
   });
 
-  // Use useMemo to ensure consistent date formatting
-  const formattedDate = useMemo(() => {
-    if (!note?.createdAt) return '';
-    return new Date(note.createdAt).toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    });
-  }, [note?.createdAt]);
+  // // Use useMemo to ensure consistent date formatting
+  // const formattedDate = useMemo(() => {
+  //   if (!note?.createdAt) return '';
+  //   return new Date(note.createdAt).toLocaleDateString('en-US', {
+  //     year: 'numeric',
+  //     month: 'long',
+  //     day: 'numeric',
+  //     hour: '2-digit',
+  //     minute: '2-digit'
+  //   });
+  // }, [note?.createdAt]);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error || !note) return <p>Something went wrong.</p>;
+  // if (isLoading) return <p>Loading...</p>;
+  // if (error || !note) return <p>Something went wrong.</p>;
 
   return (
-    <div className={css.container}>
+  <>
+    {isLoading && <p>Loading, please wait...</p>}
+      {error && <Error message="Something went wrong." />}
       {note && (
+
+      <div className={css.container}>
         <div className={css.item}>
           <div className={css.header}>
             <h2>{note.title}</h2>
             <button className={css.editBtn}>Edit note</button>
           </div>
           <div className={css.content}>{note.content}</div>
-          <div className={css.date}>{formattedDate}</div>
-          {note.tag && <p className={css.tags}>Tag: {note.tag}</p>}
+          <div className={css.date}>{note?.createdAt}</div>
+            {/* {note.tag && <p className={css.tags}>Tag: {note.tag}</p>} */}
+            
+        </div>
         </div>
       )}
-    </div>
+  </>
   );
-}
+};
 
 export default NoteDetailsClient;
