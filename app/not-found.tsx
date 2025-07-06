f610e05875d708cd43b1724fd3d51e3edc610769
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-06 21:04:19 +0300

    0607 2104

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,23 @@
 import css from './Home.module.css';
+import Link from 'next/link';
 
 export default function NotFound() {
-    return (
-        <div className={css.container}>
-            <h1 className={css.title}>404 - Page not found</h1>
-            <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
-        </div>
-    );
+  return (
+    <div className={css.container}>
+      <h2 className={css.title}>Note Not Found</h2>
+      <p className={css.description}>The note you're looking for doesn't exist.</p>
+      <Link href="/notes/filter/all">
+        ← Back to notes
+      </Link>
+    </div>
+  );
 }
+
+// export default function NotFound() {
+//     return (
+//         <div className={css.container}>
+//             <h1 className={css.title}>404 - Page not found</h1>
+//             <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
+//         </div>
+//     );
+// }
